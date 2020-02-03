e80cd4fb79ad667b28da6a66c1a9e373b06169f2
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-02-03 16:25:19 -0600

    uv index added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var cityName = "Dallas" //$("#city").val();
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=a06d53a4d8132cb2c57dac5818e92924"
 
+
+
 displayWeather();
+// displayUV();
 
 function displayWeather() {
-
-
 
 
     $.ajax({
@@ -13,6 +14,7 @@ function displayWeather() {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+
 
         var tempF = Math.round(((response.main.temp - 273.15) * 9/5 + 32));
 
@@ -22,5 +24,64 @@ function displayWeather() {
         $(".windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
 
 
+
+        function displayUV() {
+
+            var lat = response.coord.lat;
+            var lon = response.coord.lat;
+            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=a06d53a4d8132cb2c57dac5818e92924&lat=" + lat + "&lon=" + lon
+
+            console.log(lat)
+
+            $.ajax({
+                url: uvURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                var uv = response.value;
+                var uvBox = $(".uvBox");
+
+                $(".uvIndex").text("UV Index: ");
+                uvBox.text(uv)
+
+                if (uv < 3) {
+                    $(".uvBox").attr(
+                        'style', 
+                        'background-color: green'
+                    )
+                }
+
+                else if (uv > 2.999 & uv < 6) {
+                    uvBox.attr({
+                        style: 'background-color: yellow; color: black'
+                    })
+                }
+
+                else if ( uv > 5.999 && uv < 8) {
+                    uvBox.attr(
+                        'style',
+                        'background-color: orange'
+                    )
+                }
+
+                else if (uv > 7.999 && uv < 11) {
+                    uvBox.attr(
+                        'style',
+                        'background-color: red'
+                    )
+                }
+
+                else if (uv > 10.999) {
+                    uvBox.attr(
+                        'style',
+                        'background-color: purple'
+                    )
+                };
+            });
+        };
+
+        displayUV();
+
     });
 }
+
