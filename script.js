c9dc75fb252ce6986b215b9a86a5c3d2aa97ac76
kayla8693 <56415756+kayla8693@users.noreply.github.com>
2020-02-05 19:18:43 -0600

    added comments and enter=click function

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// empty array for cities; cities will be pushed into array upon search
 var cityList = [];
+
+// dates for current weather and forcast
 var a = moment();
 var b = a.clone().add(1, 'day');
 var c = a.clone().add(2, 'day');
@@ -13,6 +16,16 @@ var day3 = d.format('MM/DD/YYYY');
 var day4 = e.format('MM/DD/YYYY');
 var day5 = f.format('MM/DD/YYYY');
 
+// enter button triggers search click
+$('#cityInput').keypress(function (e) {
+    var key = e.which;
+    if (key == 13) {
+        $('#search').click();
+        return false;
+    }
+});
+
+// ajax call for current weather
 function displayWeather() {
 
     var cityName = $(this).attr('data-name');
@@ -24,19 +37,21 @@ function displayWeather() {
     }).then(function (response) {
         console.log(response);
 
-        var tempF = Math.round(((response.main.temp - 273.15) * 9/5 + 32));
+        // converts temp to F and rounds temp to nearest #; adds weather icon
+        var tempF = Math.round(((response.main.temp - 273.15) * 9 / 5 + 32));
         var iconURL = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
         var icon = $('<img>').attr('src', iconURL);
 
+        // pulling info from response and setting text in divs
         $(".city").html("<h4>" + response.name + ", " + response.sys.country + " (" + date + ")" + "</h4>");
         $('.iconBox').empty();
         $('.iconBox').append(icon);
         icon.addClass('img1')
-
         $(".temp").text("Temperature: " + tempF + " F");
         $(".humidity").text("Humidity: " + response.main.humidity + "%");
         $(".windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
-        
+
+        // uv ajax pull using coordinates from above respnse
         function displayUV() {
 
             var lat = response.coord.lat;
@@ -53,9 +68,10 @@ function displayWeather() {
                 $(".uvIndex").text("UV Index: ");
                 uvBox.text(uv)
 
+                // sets color of uvBox based on uv #
                 if (uv < 3) {
                     $(".uvBox").attr(
-                        'style', 
+                        'style',
                         'background-color: green'
                     )
                 }
@@ -66,7 +82,7 @@ function displayWeather() {
                     })
                 }
 
-                else if ( uv > 5.999 && uv < 8) {
+                else if (uv > 5.999 && uv < 8) {
                     uvBox.attr(
                         'style',
                         'background-color: orange'
@@ -89,33 +105,35 @@ function displayWeather() {
             });
         };
 
+        // 5-day forcast ajax call
         function displayForcast() {
             var country = response.sys.country;
             var forcastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "," + country + "&appid=a06d53a4d8132cb2c57dac5818e92924";
 
-            $.ajax ({
+            $.ajax({
                 url: forcastURL,
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
-                var temp1 = Math.round(((response.list[2].main.temp - 273.15) * 9/5 + 32));
-                var temp2 = Math.round(((response.list[10].main.temp - 273.15) * 9/5 + 32));
-                var temp3 = Math.round(((response.list[18].main.temp - 273.15) * 9/5 + 32));
-                var temp4 = Math.round(((response.list[26].main.temp - 273.15) * 9/5 + 32));
-                var temp5 = Math.round(((response.list[34].main.temp - 273.15) * 9/5 + 32));
+                // converting temps to F and rounding; pulling from each temp, humidity, icon from same time each day
+                var temp1 = Math.round(((response.list[2].main.temp - 273.15) * 9 / 5 + 32));
+                var temp2 = Math.round(((response.list[10].main.temp - 273.15) * 9 / 5 + 32));
+                var temp3 = Math.round(((response.list[18].main.temp - 273.15) * 9 / 5 + 32));
+                var temp4 = Math.round(((response.list[26].main.temp - 273.15) * 9 / 5 + 32));
+                var temp5 = Math.round(((response.list[34].main.temp - 273.15) * 9 / 5 + 32));
                 var icon1URL = "https://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png";
                 var icon2URL = "https://openweathermap.org/img/wn/" + response.list[10].weather[0].icon + "@2x.png";
                 var icon3URL = "https://openweathermap.org/img/wn/" + response.list[18].weather[0].icon + "@2x.png";
                 var icon4URL = "https://openweathermap.org/img/wn/" + response.list[26].weather[0].icon + "@2x.png";
                 var icon5URL = "https://openweathermap.org/img/wn/" + response.list[34].weather[0].icon + "@2x.png";
 
-
+                // empty day1 div first, then setting text info for day1
                 $('#day1').empty();
                 var date1 = $('<h6>').text(day1);
                 var icon1 = $('<img>').attr('src', icon1URL);
                 icon1.addClass('img1');
                 $('#day1').addClass('rounded');
-                $('#day1').attr('style', 'background-color: red');
+                $('#day1').attr('style', 'background-color: #037EFC');
                 var temp1Div = $('<p>').text("Temp: " + temp1 + " F");
                 var humid1 = $('<p>').text("Humidity: " + response.list[2].main.humidity + "%");
 
@@ -124,8 +142,7 @@ function displayWeather() {
                 $('#day1').append(temp1Div);
                 $('#day1').append(humid1);
 
-                
-
+                // Day2
                 $('#day2').empty();
                 var date2 = $('<h6>').text(day2);
                 var icon2 = $('<img>').attr('src', icon2URL)
@@ -140,9 +157,7 @@ function displayWeather() {
                 $('#day2').append(temp2Div);
                 $('#day2').append(humid2);
 
-
-
-
+                // Day3
                 $('#day3').empty();
                 var date3 = $('<h6>').text(day3);
                 var icon3 = $('<img>').attr('src', icon3URL)
@@ -157,9 +172,7 @@ function displayWeather() {
                 $('#day3').append(temp3Div);
                 $('#day3').append(humid3);
 
-
-
-
+                // Day4
                 $('#day4').empty();
                 var date4 = $('<h6>').text(day4);
                 var icon4 = $('<img>').attr('src', icon4URL)
@@ -174,10 +187,7 @@ function displayWeather() {
                 $('#day4').append(temp4Div);
                 $('#day4').append(humid4);
 
-
-                
-
-
+                // Day5
                 $('#day5').empty();
                 var date5 = $('<h6>').text(day5);
                 var icon5 = $('<img>').attr('src', icon5URL)
@@ -194,12 +204,13 @@ function displayWeather() {
 
             });
         };
-
+        // calling functions
         displayUV();
         displayForcast();
     });
 };
 
+// creates city buttons
 function renderButtons() {
 
     $('#cities').empty();
@@ -210,8 +221,12 @@ function renderButtons() {
         a.attr('data-name', cityList[i]);
         a.text(cityList[i].toUpperCase());
         $("#cities").prepend(a);
+
+        localStorage.setItem('city', cityList[i]);
     };
 
+
+    // clears cityList array and list of displayed cities
     $('.clear').removeClass('hide');
     $('.clear').on('click', function () {
         $('#cities').empty();
@@ -221,14 +236,15 @@ function renderButtons() {
     });
 };
 
-$("#search").on("click", function(event) {
+// upon clicking search btn, city pushed to array and renderButtons function called
+$("#search").on("click", function (event) {
     event.preventDefault();
-    
+
     var city = $("#cityInput").val().trim();
     cityList.push(city);
     console.log(cityList);
     renderButtons();
-
 });
 
+// upon click of any btn w cityBtn class, displayWeather function called
 $(document).on('click', '.cityBtn', displayWeather);
